8cffa39f192fd742a84a3469bc1a24d8948beea4
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-07-17 17:32:25 -0600

    adding in read only

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-function add(first: number, second: number): number {
-  return '';// first + second;
+type Season = 'Spring' | 'Summer' | 'Fall' | 'Winter';
+
+function nextSeason(season: Season): Season {
+  switch(season) {
+    case 'Spring': return 'Summer';
+    case 'Summer': return 'Fall';
+    case 'Fall': return 'Winter';
+    case 'Winter': return 'Spring';
+    default: throw 'No season';
+  }
 }
 
-add(5);
+nextSeason('Summer');
+
+function length(line: {start: number, end: number}): number {
+  return line.end - line.start;
+}
+
+length({start: 3, end: 6});
+
+length({start: 3, end: 6, extraParam: 10});
+
+type Line =  $ReadOnly<{
+  start: number,
+  end: number
+}>;
+
+function lineLength(line: Line): number {
+  // line.start = 10;
+  return line.end - line.start;
+}
+
+type OtherLine =  {
+  +start: number,
+  +end: number
+};
+
+function otherLineLength(line: OtherLine): number {
+  // line.start = 10;
+  return line.end - line.start;
+}
+
+// function add(first: number, second: number): number {
+//   return '';// first + second;
+// }
+
+
+
+// add(5);
