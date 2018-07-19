7c1fc990280e212adbbe14d8a332add43b7e9d27
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-07-19 17:01:33 -0600

    adding enum support to it and testing if you can add additional properties to object types

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ type Season = 'Spring' | 'Summer' | 'Fall' | 'Winter';
 
 function nextSeason(season: Season): Season {
   switch(season) {
+  // switch(typeof (season)) {
     case 'Spring': return 'Summer';
     case 'Summer': return 'Fall';
     case 'Fall': return 'Winter';
@@ -10,7 +11,36 @@ function nextSeason(season: Season): Season {
   }
 }
 
-nextSeason('Summer');
+// function lastSeason(season: Season): Season {
+//   if((typeof season) === 'Spring') {
+//     return 'Winter';
+//   }
+//   else if((typeof season) === 'Summer') {
+//     return 'Spring';
+//   }
+//   else if((typeof season) === 'Fall') {
+//     return 'Summer';
+//   }
+//   else if((typeof season) === 'Winter') {
+//     return 'Fall';
+//   }
+// }
+
+console.log(nextSeason('Summer'));
+// console.log(lastSeason('Summer'));
+
+const suits = {
+  'heart': 'heart',
+  'diamond': 'diamond',
+  'spade': 'spade',
+  'club': 'club'
+};
+
+type Suit = $Keys<typeof suits>;
+const cardSuit: Suit = 'heart';
+const otherCardSuit: Suit = suits.club;
+
+
 
 function length(line: {start: number, end: number}): number {
   return line.end - line.start;
@@ -29,6 +59,21 @@ function lineLength(line: Line): number {
   // line.start = 10;
   return line.end - line.start;
 }
+
+lineLength({
+  start: 2,
+  end: 2
+});
+
+lineLength({
+  start: 2,
+  end: 2,
+  extraParam: 10
+});
+
+// let line = Line(2, 2);
+// lineLength(line);
+
 
 type OtherLine =  {
   +start: number,
